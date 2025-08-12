85f7498fb77fde1b425ce30ba620d1c1f26b706c
Arman Foroughi <foroughiarman7@gmail.com>
2025-08-13 00:06:22 +0200

    Add files via upload

---
+++ linknet_efficientnetb3.py
@@ -0,0 +1,301 @@
+import os
+
+import cv2
+import tensorflow
+import tensorflow.keras as keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+DATA_DIR = "seg1-2"
+IMG_SIZE = 320
+
+x_train_dir = os.path.join(DATA_DIR, 'train/images')
+y_train_dir = os.path.join(DATA_DIR, 'train/masks')
+
+x_valid_dir = os.path.join(DATA_DIR, 'valid/images')
+y_valid_dir = os.path.join(DATA_DIR, 'valid/masks')
+
+x_test_dir = os.path.join(DATA_DIR, 'test/images')
+y_test_dir = os.path.join(DATA_DIR, 'test/masks')
+
+# helper function for data visualization
+def visualize(**images):
+    """PLot images in one row."""
+    n = len(images)
+    plt.figure(figsize=(16, 5))
+    for i, (name, image) in enumerate(images.items()):
+        plt.subplot(1, n, i + 1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.title(' '.join(name.split('_')).title())
+        plt.imshow(image)
+    plt.show()
+    
+# helper function for data visualization    
+def denormalize(x):
+    """Scale image to range 0..1 for correct plot"""
+    x_max = np.percentile(x, 98)
+    x_min = np.percentile(x, 2)    
+    x = (x - x_min) / (x_max - x_min)
+    x = x.clip(0, 1)
+    return x
+    
+class Dataset:
+   
+    CLASSES = ['crack']
+    
+    def __init__(
+            self, 
+            images_dir, 
+            masks_dir, 
+            classes=None, 
+            augmentation=None, 
+            preprocessing=None,
+    ):
+        self.list_img = os.listdir(images_dir)
+        self.list_img.sort()
+        self.list_mask = os.listdir(masks_dir)
+        self.list_mask.sort()
+        self.images_fps = [os.path.join(images_dir, image_id) for image_id in self.list_img]
+        self.masks_fps = [os.path.join(masks_dir, mask_id) for mask_id in self.list_mask]
+        
+        # convert str names to class values on masks
+        self.class_values = [self.CLASSES.index(cls.lower()) for cls in classes]
+        
+        self.augmentation = augmentation
+        self.preprocessing = preprocessing
+    
+    def __getitem__(self, i):
+        new_width, new_height = IMG_SIZE, IMG_SIZE
+        
+        # read data
+        image = cv2.imread(self.images_fps[i])
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = cv2.resize(image, (new_width, new_height))
+
+        mask = cv2.imread(self.masks_fps[i], cv2.IMREAD_GRAYSCALE)
+        mask = cv2.resize(mask, (new_width, new_height), interpolation=cv2.INTER_NEAREST)
+        
+        mask = mask.astype('float32')  # یا np.float32
+        mask = np.expand_dims(mask, axis=-1)
+        
+        # apply augmentations
+        if self.augmentation:
+            sample = self.augmentation(image=image, mask=mask)
+            image, mask = sample['image'], sample['mask']
+        
+        # apply preprocessing
+        if self.preprocessing:
+            sample = self.preprocessing(image=image, mask=mask)
+            image, mask = sample['image'], sample['mask']
+        
+        return image, mask
+        
+    def __len__(self):
+        return len(self.list_img)
+    
+    
+class Dataloder(keras.utils.Sequence):
+
+    def __init__(self, dataset, batch_size=1, shuffle=False):
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.indexes = np.arange(len(dataset))
+
+        self.on_epoch_end()
+
+    def __getitem__(self, i):
+        
+        # collect batch data
+        start = i * self.batch_size
+        stop = (i + 1) * self.batch_size
+        data = []
+        for j in range(start, stop):
+            data.append(self.dataset[j])
+        
+        # transpose list of lists
+        batch = [np.stack(samples, axis=0) for samples in zip(*data)]
+        
+        return batch
+    
+    def __len__(self):
+        """Denotes the number of batches per epoch"""
+        return len(self.indexes) // self.batch_size
+    
+    def on_epoch_end(self):
+        """Callback function to shuffle indexes each epoch"""
+        if self.shuffle:
+            self.indexes = np.random.permutation(self.indexes)   
+
+dataset = Dataset(x_train_dir, y_train_dir, classes=['crack'])
+
+image, mask = dataset[5] 
+visualize(
+    image=image, 
+    crack_mask=mask[..., 0].squeeze()
+)
+
+import albumentations as A
+
+def round_clip_0_1(x, **kwargs):
+    return x.round().clip(0, 1)
+
+# define heavy augmentations
+def get_training_augmentation():
+    train_transform = [
+
+        A.HorizontalFlip(p=0.5),
+
+        A.ShiftScaleRotate(scale_limit=0.5, rotate_limit=0, shift_limit=0.1, p=1, border_mode=0),
+
+        A.PadIfNeeded(min_height=IMG_SIZE, min_width=IMG_SIZE, always_apply=True, border_mode=0),
+        A.RandomCrop(height=IMG_SIZE, width=IMG_SIZE, always_apply=True),
+
+        A.GaussNoise(var_limit=(10.0, 50.0), p=0.2),
+        A.Perspective(scale=(0.05, 0.1), keep_size=True, fit_output=False, p=0.5),
+
+        A.OneOf(
+            [
+                A.CLAHE(p=1),
+                A.RandomBrightness(p=1),
+                A.RandomGamma(p=1),
+            ],
+            p=0.9,
+        ),
+
+        A.OneOf(
+            [
+                A.Sharpen(alpha=(0.2, 0.5), lightness=(0.5, 1.0), p=1),
+                A.Blur(blur_limit=3, p=1),
+                A.MotionBlur(blur_limit=3, p=1),
+            ],
+            p=0.9,
+        ),
+
+        A.OneOf(
+            [
+                A.RandomContrast(p=1),
+                A.HueSaturationValue(p=1),
+            ],
+            p=0.9,
+        ),
+        A.Lambda(mask=round_clip_0_1)
+    ]
+    return A.Compose(train_transform)
+
+
+def get_validation_augmentation():
+    test_transform = [
+        A.PadIfNeeded(IMG_SIZE, IMG_SIZE)
+    ]
+    return A.Compose(test_transform)
+
+def get_preprocessing(preprocessing_fn):
+    _transform = [
+        A.Lambda(image=preprocessing_fn),
+    ]
+    return A.Compose(_transform)
+
+import os
+os.environ['SM_FRAMEWORK'] = 'tf.keras'
+
+import segmentation_models as sm
+from segmentation_models import Linknet
+from segmentation_models.losses import DiceLoss
+from segmentation_models.losses import BinaryFocalLoss
+from tensorflow.keras.optimizers import Adam
+
+BACKBONE = 'efficientnetb3'
+BATCH_SIZE = 4
+CLASSES = ['crack']
+LR = 0.0001
+EPOCHS = 20
+
+# define network parameters
+n_classes = 1
+activation = 'sigmoid'
+
+model = Linknet(
+    backbone_name=BACKBONE,
+    input_shape=(IMG_SIZE, IMG_SIZE, 3),
+    classes=n_classes,
+    activation=activation
+)
+
+preprocess_input = sm.get_preprocessing(BACKBONE)
+metrics = [sm.metrics.IOUScore(threshold=0.5), sm.metrics.FScore(threshold=0.5)]
+total_loss = DiceLoss() + BinaryFocalLoss()
+
+model.compile(
+    optimizer=Adam(LR),
+    loss=total_loss,  
+    metrics=metrics
+)
+
+print("compile done")
+
+# Dataset for train images
+train_dataset = Dataset(
+    x_train_dir, 
+    y_train_dir, 
+    classes=CLASSES, 
+    augmentation=get_training_augmentation(),
+    preprocessing=get_preprocessing(preprocess_input),
+)
+
+# Dataset for validation images
+valid_dataset = Dataset(
+    x_valid_dir, 
+    y_valid_dir, 
+    classes=CLASSES, 
+    augmentation=get_validation_augmentation(),
+    preprocessing=get_preprocessing(preprocess_input),
+)
+
+train_dataloader = Dataloder(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+valid_dataloader = Dataloder(valid_dataset, batch_size=1, shuffle=False)
+
+print(train_dataloader[0][0].shape)
+
+# check shapes for errors
+assert train_dataloader[0][0].shape == (BATCH_SIZE, IMG_SIZE, IMG_SIZE, 3)
+assert train_dataloader[0][1].shape == (BATCH_SIZE, IMG_SIZE, IMG_SIZE, n_classes)
+
+# define callbacks for learning rate scheduling and best checkpoints saving
+callbacks = [
+    keras.callbacks.ModelCheckpoint('./best_model.h5', save_weights_only=True, save_best_only=True, mode='min'),
+    keras.callbacks.ReduceLROnPlateau(),
+]
+
+# train model
+history = model.fit_generator(
+    train_dataloader, 
+    steps_per_epoch=len(train_dataloader), 
+    epochs=EPOCHS, 
+    callbacks=callbacks, 
+    validation_data=valid_dataloader, 
+    validation_steps=len(valid_dataloader),
+)
+
+# Plot training & validation iou_score values
+plt.figure(figsize=(30, 5))
+plt.subplot(121)
+plt.plot(history.history['iou_score'])
+plt.plot(history.history['val_iou_score'])
+plt.title('Model iou_score')
+plt.ylabel('iou_score')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+
+# Plot training & validation loss values
+plt.subplot(122)
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.savefig("fig.png")
+
+print("save image")
